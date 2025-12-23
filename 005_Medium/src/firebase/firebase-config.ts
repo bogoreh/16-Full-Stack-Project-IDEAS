2967eb45726ea743723e52dc092f6a4bddfee4c1
bogoreh <76124689+bogoreh@users.noreply.github.com>
2025-12-23 10:47:25 +0300

    first initial

---
+++ 005_Medium/src/firebase/firebase-config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAD2_5njZVMcPFmlymF-t590iuUbmKaSSc",
+  authDomain: "medium-clone-469b9.firebaseapp.com",
+  projectId: "medium-clone-469b9",
+  storageBucket: "medium-clone-469b9.appspot.com",
+  messagingSenderId: "176786971428",
+  appId: "1:176786971428:web:6919ab946aa228ef0bb1b1",
+  measurementId: "G-4PCF1WCZQ6",
+};
+
+export default firebaseConfig;
